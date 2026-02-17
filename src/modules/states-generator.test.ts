3e24fdcf7c610ad79ab81c2f8a7f6a131e079c40
Sami Al Saati <sami.alsaati@hotmail.com>
2026-02-17 21:05:12 +0100

    added continues if list is empty test case for defineStateForms

--- src/modules/states-generator.test.ts
+++ src/modules/states-generator.test.ts
@@ -22,6 +22,23 @@ describe("defineStateForms" , () => {
             expect(pack.states[0].form).toBeUndefined();
         });
 
-
     });
+    //If list is empty, it will continue to the next iteration. Since there is only 1 element in states it will skip the for loop.
+    //Thus, no fullName or formName is set.
+    describe("when list is empty", () => {
+        it("continues",() => {
+            (globalThis as any).TIME = false
+
+            const states = [{i:1, name: "test", removed: false, lock: false}];
+
+            (globalThis as any).pack = {states: states}
+
+            window.States.defineStateForms([]);
+
+            expect(pack.states[0].formName).toBeUndefined()
+            expect(pack.states[0].fullName).toBeUndefined()
+        })
+
+
+    })
 });
