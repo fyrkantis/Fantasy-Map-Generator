84c43a7325ca0c9f27a4ec825b5071b897b1cde1
David Kniberg <david@kniberg.com>
2026-03-02 21:01:31 +0100

    Fix: Consistent indentation for clean patch
    I also ran the linter.

--- src/modules/biome-layer.test.ts
+++ src/modules/biome-layer.test.ts
@@ -1,34 +1,33 @@
 // @vitest-environment jsdom
-import {describe, it, expect} from "vitest";
+import { describe, it, expect } from "vitest";
 import "./biomes";
 
 describe("When the height is less than 20", () => {
-    it("returns 0 for all water cells: marine biome", () => {
-        expect((globalThis as any).Biomes.getId(0, 0, 10, false)).toBe(0);
-    })
-})
+  it("returns 0 for all water cells: marine biome", () => {
+    expect((globalThis as any).Biomes.getId(0, 0, 10, false)).toBe(0);
+  });
+});
 
-describe("When the temperature is less than -5", () =>{
-    it("returns 11 for too cold: permafrost biome", ()=> {
-        expect((globalThis as any).Biomes.getId(21, -6, 21, false)).toBe(11);
-    })
-})
+describe("When the temperature is less than -5", () => {
+  it("returns 11 for too cold: permafrost biome", () => {
+    expect((globalThis as any).Biomes.getId(21, -6, 21, false)).toBe(11);
+  });
+});
 
 describe("When the temperature is more than or equal to 25 and has no river and moisture is less than 8", () => {
-    it("returns 1 for too hot and dry: hot desert biome", () => {
-        expect((globalThis as any).Biomes.getId(7, 25, 21, false)).toBe(1);
-    })
-})
+  it("returns 1 for too hot and dry: hot desert biome", () => {
+    expect((globalThis as any).Biomes.getId(7, 25, 21, false)).toBe(1);
+  });
+});
 
 describe("When the moisture is more than 40 and height is less than 25", () => {
-    it("returns 12 for too wet: wetland biome", () => {
-        expect((globalThis as any).Biomes.getId(41, 20, 24, false)).toBe(12);
-    })
-})
+  it("returns 12 for too wet: wetland biome", () => {
+    expect((globalThis as any).Biomes.getId(41, 20, 24, false)).toBe(12);
+  });
+});
 
 describe("When the moisture is more than 24 and height is more than 24 and less than 60", () => {
-    it("returns 12 for too wet: wetland biome", () => {
-        expect((globalThis as any).Biomes.getId(25, 20, 25, false)).toBe(12);
-    })
-})
-
+  it("returns 12 for too wet: wetland biome", () => {
+    expect((globalThis as any).Biomes.getId(25, 20, 25, false)).toBe(12);
+  });
+});
